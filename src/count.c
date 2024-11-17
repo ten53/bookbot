d6f3f3e32f741deb796f64e3929b33a814059812
Kenneth Wright <kenneth.wright@ten53.net>
2024-11-17 09:26:20 +0100

    Added word count function

--- src/count.c
+++ src/count.c
@@ -4,25 +4,32 @@
 #include <stdbool.h>
 #include "count.h"
 
+
 bool is_delimiter(char c) {
     return (isspace(c) || strchr(".?!,;:()[]{}-\"/", c));
 }
 
-int  word_count(char* file_content) {
+/**
+ * Iterates through a null-terminated string sequence and counts
+ * the number of words based on alphabetic sequences (A-Z, a-z) separated by
+ * delimiters (space, punctution).
+ * Targets English language texts (apostrophes are included e.g don't, it's).
+*/
+int  word_count(char* text) {
 
-    if (!file_content) {
-        fprintf(stderr, "Error: Empty buffer\n");
+    if (!text) {
+        fprintf(stderr, "Error: Empty text buffer\n");
         return 0;
     }
 
     int word_count = 0;
     bool IN_WORD = false;
 
-    for (int i = 0; file_content[i] != '\0'; i++) {
+    for (int i = 0; text[i] != '\0'; i++) {
         // allow apostrophe inside word e.g. don't
-        if (isalpha(file_content[i]) || (file_content[i] == '\'' && i > 0 && isalpha(file_content[i - 1]) && isalpha(file_content[i + 1]))) {
+        if (isalpha(text[i]) || (text[i] == '\'' && i > 0 && isalpha(text[i - 1]) && isalpha(text[i + 1]))) {
             if (!IN_WORD) IN_WORD = true;
-        } else if (is_delimiter(file_content[i])) {
+        } else if (is_delimiter(text[i])) {
             if (IN_WORD) {
                 word_count++;
                 IN_WORD = false;
